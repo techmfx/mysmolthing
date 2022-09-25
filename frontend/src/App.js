33adcaadaad0cd24e29234c03c80e2bd961f5ea3
tmf <96436333+techmfx@users.noreply.github.com>
2022-09-25 16:01:26 +0800

    Different behaviour for different screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Spline from '@splinetool/react-spline';
-import smol from "./smol.spline";
+import Spline from "@splinetool/react-spline";
+import { BrowserView, MobileView } from "react-device-detect";
+import smol_desktop from "./smol_desktop.spline";
+import smol_mobile from "./smol_mobile.spline";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Spline 
-        scene={smol}
-      />
-    </div>
+    <>
+      <BrowserView className="App">
+        <Spline scene={smol_desktop} />
+      </BrowserView>
+      <MobileView className="App">
+        <Spline scene={smol_mobile} />
+      </MobileView>
+    </>
   );
 }
 
